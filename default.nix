6164bb0009f6b5f2c634225b07bc0fd4a1902bec
Tikhon Jelvis <tikhon.jelvis@target.com>
2020-05-30 14:02:59 -0700

    Got the buildAndWatch command to work with the newer cabal version.

--- default.nix
+++ default.nix
@@ -37,7 +37,7 @@ let
       });
     }).overrideAttrs (old: {
       shellHook = ''
-        alias buildAndWatch="cabal configure && cabal build && ./dist/build/site/site clean && ./dist/build/site/site watch"
+        alias buildAndWatch="cabal configure && cabal build && cabal exec site -- clean && cabal exec site -- watch"
         echo ""
         echo "  Haskell.org Dev Shell"
         echo "    \`buildAndWatch\` to serve the site, and rebuild when files change."
