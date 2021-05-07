6a373d46e52e6b2dc86e6dbaeea5572a5a32d639
Tikhon Jelvis <tikhon@jelv.is>
2021-05-07 12:39:11 -0700

    Made linkchecker optional in default.nix

--- default.nix
+++ default.nix
@@ -3,7 +3,8 @@
 , pkgs     ?
     import (builtins.fetchTarball {
       url    = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
-      }) {}
+    }) {}
+, doCheck ? true
 }:
 
 let
@@ -51,6 +52,8 @@ let
 
   built = pkgs.stdenv.mkDerivation {
     name = "haskell.org";
+    inherit doCheck;
+
     src = gitignore.gitignoreSource [
       ".git"
       "*.cabal"
@@ -58,22 +61,18 @@ let
       ".github"
       ] ./.;
     buildInputs = [ builder pkgs.linkchecker ];
+
     LOCALE_ARCHIVE = "${pkgs.glibcLocales}/lib/locale/locale-archive";
     LC_ALL = "C.UTF-8";
-    installPhase = ''
-      echo ""
-      echo "  Building static site..."
-      echo ""
-      haskell-org-site build
-      echo ""
-      echo "  Checking for bad links..."
-      echo ""
+
+    buildPhase = ''
+      ${builder}/bin/haskell-org-site build
+    '';
+    checkPhase = ''
       linkchecker _site
-      echo ""
-      echo "  Copying static site to $out..."
+    '';
+    installPhase = ''
       cp -r _site $out
-      echo "  Build complete"
-      echo ""
     '';
   };
 in
