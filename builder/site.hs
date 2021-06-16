e98db12c365b4f26a18ea50808ce565b58325377
rebecca skinner <rebecca@rebeccaskinner.net>
2021-06-16 00:23:03 -0500

    building stub json of all testimonials

--- builder/site.hs
+++ builder/site.hs
@@ -4,7 +4,15 @@ import           Data.Monoid           ((<>))
 import           Data.Time.Calendar
 import           Data.Time.Clock
 import           Hakyll
+import           Hakyll.Core.Routes
+import           Hakyll.Core.Compiler
+import           Hakyll.Core.Compiler.Internal
+import           Hakyll.Core.Provider
 import           System.FilePath.Posix
+import           Data.Aeson
+import qualified Data.ByteString.Lazy as BL
+import Testimonial
+import Control.Monad
 
 
 --------------------------------------------------------------------------------
@@ -37,6 +45,30 @@ main = mkContext >>= \ctx -> hakyll $ do
   match "templates/*" $
     compile templateCompiler
 
+  match "testimonials/logos/*" $ do
+    route idRoute
+    compile copyFileCompiler
+
+  match "testimonials/*.yaml" $ do
+    compile parseTestimonialCompiler
+
+  create ["testimonials.json"] $ do
+    route idRoute
+    compile $ do
+      unsafeCompiler $ putStrLn "inside of create testimonials compiler"
+      testimonials <- loadAll "testimonials/*.yaml" :: Compiler [Item Testimonial]
+      unsafeCompiler $ putStrLn $ "testimonials: " <> show testimonials
+      item <- makeItem $ (BL.unpack . encode . map itemBody) testimonials
+      saveSnapshot "_final" item
+      pure item
+
+parseTestimonialCompiler :: Compiler (Item Testimonial)
+parseTestimonialCompiler = do
+  identifier  <- getUnderlying
+  provider    <- compilerProvider <$> compilerAsk
+  body        <- unsafeCompiler $ BL.readFile (resourceFilePath provider identifier)
+  testimonial <- parseTestimonialM body
+  makeItem testimonial
 
 mdCompiler :: Context String -> Compiler (Item String)
 mdCompiler ctx =
@@ -45,13 +77,11 @@ mdCompiler ctx =
   >>= loadAndApplyTemplate "templates/default.html" ctx
   >>= relativizeUrls
 
-
 defCompiler :: Context String -> Compiler (Item String)
 defCompiler ctx = getResourceBody
   >>= applyAsTemplate ctx
   >>= loadAndApplyTemplate "templates/default.html" ctx
   >>= relativizeUrls
-
 
 mkContext :: IO (Context String)
 mkContext = do
@@ -59,7 +89,6 @@ mkContext = do
   return $ constField "year" (show year)
         <> dropIndexHtml
         <> defaultContext
-
 
 cleanRoute :: Routes
 cleanRoute = customRoute $
