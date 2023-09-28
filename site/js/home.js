ee48108fdb17a81ba915d25635a037ae59ac6fca
Enmanuel Carvajal <enmanuelcarvajal@proton.me>
2023-09-28 15:47:43 -0400

    change copy on collapse  feat

--- site/js/home.js
+++ site/js/home.js
@@ -4,7 +4,23 @@ $(function () {
 });
 
 function handleFeaturesCollapse() {
+  // Allows the area/wrapper around feature copy to toggle collapse,
+  // not just the expand link as set in the markup
+  // https://getbootstrap.com/docs/4.0/components/collapse/#via-data-attributes
   $(".features .col-md-6").click(function () {
     $(this).find(".collapse").collapse("toggle");
+  });
+
+  const ELEMENT_SHOWN_EVENT = "shown.bs.collapse";
+  const ELEMENT_HIDDEN_EVENT = "hidden.bs.collapse";
+
+  $(".collapse").on(ELEMENT_HIDDEN_EVENT, function () {
+    const featureSelector = $(this).context.id;
+    $(`.features a[href="#${featureSelector}"]`).text("Show more");
+  });
+
+  $(".collapse").on(ELEMENT_SHOWN_EVENT, function () {
+    const featureSelector = $(this).context.id;
+    $(`.features a[href="#${featureSelector}"]`).text("Show less");
   });
 }
